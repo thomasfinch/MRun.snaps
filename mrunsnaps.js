c4dd11079873a9f98c016eb792a300c5ef96b37c
thomasfinch <tomf64@gmail.com>
2015-04-21 12:45:33 -0400

    reorganized code slightly

--- mrunsnaps.js
+++ mrunsnaps.js
@@ -1,23 +1,48 @@
 var fs = require('fs');
-var zlib = require('zlib');
-var gzip = zlib.createGzip();
+// var zlib = require('zlib');
+// var gzip = zlib.createGzip();
 var snapchat = require('snapchat');
 var client = snapchat.Client();
 var credentials = require('./credentials.js');
+
+const MIN_CHECK_TIME = 270; //4:30 in seconds
+const MAX_CHECK_TIME = 300; //5 mins in seconds
 
 // Make sure the images folder exists
 if(!fs.existsSync('./images')) {
     fs.mkdirSync('./images');
 }
 
+// Make sure the image archive folder exists
+if(!fs.existsSync('./imageArchive')) {
+    fs.mkdirSync('./imageArchive');
+}
+
+// Global Error Handler
+process.on('uncaughtException', function (err) {
+	console.error(err.stack);
+});
+
 login();
 
-function login() {
-	//Check if logged in (by test call)
-	//If so, download any new snaps, otherwise log in
+function log(message) {
+	console.log(new Date() + ': ' + message);
+}
 
+function login() {
+	log('Trying to log in');
+	client.login(credentials.username, credentials.password).then(function(data) {
+		log('Logged in');
+		getNewSnaps(); //Get new snaps now that we're logged in
+	}, function(err) {
+		log('Error logging in: ');
+		console.log(err);
+	});
+}
+
+function getNewSnaps() {
+	log('Checking for new snaps');
 	client.getSnaps().then(function(snaps) {
-		console.log(new Date()+': '+'Checking for new snaps');
 
 		// Handle any problems, such as wrong password
 	    if (typeof snaps === 'undefined') {
@@ -32,7 +57,7 @@ function login() {
 	    });
 
 	    if (snapsCount == 0) {
-	    	console.log(new Date()+': '+'No new snaps');
+	    	log('No new snaps');
 	    	return;
 	    }
 
@@ -41,7 +66,7 @@ function login() {
 	    snaps.forEach(function(snap) {
 	        // Make sure the snap item is unopened and sent to you (not sent by you)
 	        if (typeof snap.sn !== 'undefined' && typeof snap.t !== 'undefined' && snap.st == 1 && (snap.m == 0 || snap.m == 1 || snap.m == 2)) {
-	            console.log(new Date()+': '+'Saving snap from ' + snap.sn + '...');
+	            log('Saving snap from ' + snap.sn + '...');
 
 	            // Save the image to ./images/{SENDER USERNAME}_{SNAP ID}.fileExtension
 	            var fileExtension = (snap.m == 0) ? 'jpg' : 'mp4';
@@ -55,7 +80,7 @@ function login() {
 					blob.on('close', function() {
 						snapsDownloaded++;
 						if (snapsDownloaded == snapsCount) {
-							console.log(new Date()+': '+'Downloaded ' + snapsDownloaded + ' new snaps');
+							log('Downloaded ' + snapsDownloaded + ' new snaps');
 							client.clear(); //Mark all snaps as read
 							uploadSnapsToStory();
 						}
@@ -67,25 +92,23 @@ function login() {
 	    });
 
 	}, function(err) {
-		client.login(credentials.username, credentials.password).then(function(data) {
-			console.log(new Date()+': '+'Logged in');
-			login(); //Get new snaps now that we're logged in
-		}, function(err) {
-			consolelog(new Date()+': '+'Error logging in: '+err);
-		});
+		log('Error getting snaps');
+		login();
 	});
 
-	setTimeout(login, Math.floor((Math.random() * 50000)) + 45000); //Check for and download new snaps every 45-80 seconds
+	setTimeout(login, Math.floor((Math.random() * (MAX_CHECK_TIME - MIN_CHECK_TIME)*1000)) + MIN_CHECK_TIME*1000); //Check for and download new snaps within the time interval
 }
 
 function uploadSnapsToStory() {
+	//Get list of files in the images directory
 	var snapFiles = fs.readdirSync('./images/');
 	if (snapFiles.indexOf('.DS_Store') != -1)
 		snapFiles.splice(snapFiles.indexOf('.DS_Store'), 1); //Remove .DS_store from the files list (if on a Mac)
 	var numUploaded = 0;
 
+	//Upload each image one by one
 	snapFiles.forEach(function(filename) {
-		console.log(new Date()+': '+'Uploading snap: ' + filename);
+		log('Uploading snap: ' + filename);
 
 		var isVideo = (filename.substring(filename.length - 3, filename.length) != 'jpg');
 		var viewingTime = filename.substring(filename.indexOf('-') + 1, filename.indexOf('.'));
@@ -95,10 +118,14 @@ function uploadSnapsToStory() {
 
 		client.upload(blob, isVideo).then(function(mediaId) {
             client.addToStory(mediaId, viewingTime).catch(function(result) {
-        		console.log(new Date()+': '+'Sent snap ' + filename + ' to story');
-            	fs.unlinkSync('./images/' + filename); //Delete the downloaded snap
+            	//On successful upload, delete the downloaded snap
+        		log('Sent snap ' + filename + ' to story');
+        		fs.createReadStream('./images/' + filename).pipe(fs.createWriteStream('./imageArchive/' + filename)).on('close', function() { //Archive each image, useful for blocking people etc.
+        			fs.unlinkSync('./images/' + filename); //Delete the downloaded snap
+        		});
+
             	if (++numUploaded == snapFiles.length) {
-            		console.log(new Date()+': '+'Done uploading snaps');
+            		log('Done uploading snaps');
             	}
             });
 
